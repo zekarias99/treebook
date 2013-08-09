1b86df1021add690bd36587d16d31777631b6358
Zekarias Araya <zekarias99@gmail.com>
2013-08-09 14:32:12 -0700

    Add migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Treebook::Application.routes.draw do
+  resources :statuses
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
